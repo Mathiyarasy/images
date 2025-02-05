023a979886c55a12074702d6d88ae6dbd9c69f98
Mathiyarasy <157102811+Mathiyarasy@users.noreply.github.com>
2025-02-05 22:05:29 +0000

    check same no cache for devcontainer build with nocache

--- .github/actions/smoke-test/build.sh
+++ .github/actions/smoke-test/build.sh
@@ -10,7 +10,10 @@ npm install -g @devcontainers/cli
 echo "(*) Building image - ${IMAGE}"
 id_label="test-container=${IMAGE}"
 if [ $IMAGE == "universal" ]; then
-    devcontainer up --id-label ${id_label} --workspace-folder "src/${IMAGE}/" --build-no-cache
+    #devcontainer up --id-label ${id_label} --workspace-folder "src/${IMAGE}/" --build-no-cache
+
+    devcontainer build --workspace-folder "src/${IMAGE}/" --no-cache
+
 else
     devcontainer up --id-label ${id_label} --workspace-folder "src/${IMAGE}/"
 fi

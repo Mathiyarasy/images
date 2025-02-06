6c2c1aca1dc8afe9dea8162079f6d21c1652b150
Mathiyarasy <157102811+Mathiyarasy@users.noreply.github.com>
2025-02-06 09:55:45 +0000

    test the values passed from remote

--- .github/actions/smoke-test/build.sh
+++ .github/actions/smoke-test/build.sh
@@ -9,12 +9,12 @@ npm install -g @devcontainers/cli
 
 echo "(*) Building image - ${IMAGE}"
 id_label="test-container=${IMAGE}"
+
+echo "USER_UID: $USER_UID"
+echo "USER_GID: $USER_GID"
+
 if [ $IMAGE == "universal" ]; then
     devcontainer up --id-label ${id_label} --workspace-folder "src/${IMAGE}/" 
-    
-    #--build-no-cache
-
-    #devcontainer build --workspace-folder "src/${IMAGE}/" --no-cache
 
 else
     devcontainer up --id-label ${id_label} --workspace-folder "src/${IMAGE}/"

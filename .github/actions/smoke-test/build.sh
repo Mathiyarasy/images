7c53fa74adaf0cc2c695fff620817f0afa8fff18
Mathiyarasy <157102811+Mathiyarasy@users.noreply.github.com>
2025-02-06 06:31:27 +0000

    test

--- .github/actions/smoke-test/build.sh
+++ .github/actions/smoke-test/build.sh
@@ -10,9 +10,9 @@ npm install -g @devcontainers/cli
 echo "(*) Building image - ${IMAGE}"
 id_label="test-container=${IMAGE}"
 if [ $IMAGE == "universal" ]; then
-    #devcontainer up --id-label ${id_label} --workspace-folder "src/${IMAGE}/" --build-no-cache
+    devcontainer up --id-label ${id_label} --workspace-folder "src/${IMAGE}/" --build-no-cache
 
-    devcontainer build --workspace-folder "src/${IMAGE}/" --no-cache
+    #devcontainer build --workspace-folder "src/${IMAGE}/" --no-cache
 
 else
     devcontainer up --id-label ${id_label} --workspace-folder "src/${IMAGE}/"
